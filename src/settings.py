2502d5c5393531492bf7bfc1414f9b9d2c7e7537
Ars Vladimirov <grokhael@gmail.com>
2023-11-08 16:46:35 +0600

    update summary

--- src/settings.py
+++ src/settings.py
@@ -121,7 +121,7 @@ def get_settings():
     settings["paper"] = PAPER
     settings["citation_url"] = CITATION_URL
     settings["authors"] = AUTHORS
-    settings["authors_contacts"] = AUTHORS_CONTACTS    
+    settings["authors_contacts"] = AUTHORS_CONTACTS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
     settings["slytagsplit"] = SLYTAGSPLIT
